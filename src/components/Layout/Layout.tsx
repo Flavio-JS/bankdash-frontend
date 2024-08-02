b101e5bb4d583d44d02629619263b9c538905405
Flavio-JS <flavim.filho007@gmail.com>
2024-08-02 18:06:34 -0300

    finalizado sideBar

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,69 +1,19 @@
-"use client";
-
 import { type ReactNode } from "react";
 import { Header } from "./Header/Header";
-import Link from "next/link";
-import { Icons } from "@/components/Icons/Icons";
-import { usePathname } from "next/navigation";
+import { SideBar } from "@/components/Layout/SideBar/SideBar";
 
 export type LayoutProps = {
   children: ReactNode;
 };
 
 export const Layout = ({ children }: LayoutProps) => {
-  const pathname = usePathname();
-
   return (
     <div className="flex min-h-svh flex-col bg-background">
       <div className="flex flex-1">
-        <aside className="hidden w-[230px] border-r lg:flex lg:flex-col">
-          <div className="flex h-[104px] items-center gap-2 p-8">
-            <Icons.BankDashIcon />
-            <span className="text-2xl font-bold">BankDash.</span>
-          </div>
-          <nav>
-            <ul className="p-8">
-              <li className="flex gap-6">
-                <Icons.HomeIcon
-                  fill={pathname.includes("/dashboard") ? "#2D60FF" : undefined}
-                />
-                <Link href="/dashboard">Dashboard</Link>
-              </li>
-              <li className="flex gap-6">
-                <Icons.TransactionsIcon
-                  fill={
-                    pathname.includes("/transactions") ? "#2D60FF" : undefined
-                  }
-                />
-                <Link href="/transactions">Transactions</Link>
-              </li>
-              <li>
-                <Link href="/accounts">Accounts</Link>
-              </li>
-              <li>
-                <Link href="/investments">Investments</Link>
-              </li>
-              <li>
-                <Link href="/credit-cards">Credit Cards</Link>
-              </li>
-              <li>
-                <Link href="/loans">Loans</Link>
-              </li>
-              <li>
-                <Link href="/services">Services</Link>
-              </li>
-              <li>
-                <Link href="/privileges">My Privileges</Link>
-              </li>
-              <li>
-                <Link href="/settings">Setting</Link>
-              </li>
-            </ul>
-          </nav>
-        </aside>
-        <div className="flex w-full flex-col">
+        <SideBar />
+        <div className="flex max-h-dvh w-full flex-col">
           <Header />
-          <main>{children}</main>
+          <div className="h-full overflow-y-scroll">{children}</div>
         </div>
       </div>
     </div>
---
+++ src/components/Layout/SideBar/SideBar.tsx
@@ -0,0 +1,114 @@
+"use client";
+
+import { Icons } from "@/components/Icons/Icons";
+import { SheetHeader, SheetTitle } from "@/components/ui/sheet";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+type NavIconProps = {
+  iconName: keyof typeof Icons;
+  selected: boolean;
+};
+
+const NavIcon = ({ iconName, selected }: NavIconProps) => {
+  const Icon = Icons[iconName];
+
+  return (
+    <>
+      <Icon fill={selected ? "#2D60FF" : undefined} />
+    </>
+  );
+};
+
+type ListItemProps = {
+  iconName: keyof typeof Icons;
+  href: string;
+  title: string;
+};
+
+const ListItem = ({ iconName, href, title }: ListItemProps) => {
+  const pathname = usePathname();
+
+  return (
+    <li className="flex items-center justify-start gap-6">
+      <NavIcon iconName="SelectedTabIcon" selected={pathname.includes(href)} />
+      <NavIcon iconName={iconName} selected={pathname.includes(href)} />
+      <Link
+        href={href}
+        style={{
+          color: pathname.includes(href) ? "#2D60FF" : "#B1B1B1",
+        }}
+      >
+        {title}
+      </Link>
+    </li>
+  );
+};
+
+type MenuItem = {
+  iconName: keyof typeof Icons;
+  href: string;
+  title: string;
+};
+
+const menuItems: MenuItem[] = [
+  { iconName: "DashboardIcon", href: "/dashboard", title: "Dashboard" },
+  {
+    iconName: "TransactionsIcon",
+    href: "/transactions",
+    title: "Transactions",
+  },
+  { iconName: "AccountsIcon", href: "/accounts", title: "Accounts" },
+  { iconName: "InvestmentsIcon", href: "/investments", title: "Investments" },
+  { iconName: "CreditCardsIcon", href: "/credit-cards", title: "Credit Cards" },
+  { iconName: "LoansIcon", href: "/loans", title: "Loans" },
+  { iconName: "ServicesIcon", href: "/services", title: "Services" },
+  { iconName: "MyPrivilegesIcon", href: "/privileges", title: "My Privileges" },
+  { iconName: "SettingIcon", href: "/settings", title: "Settings" },
+];
+
+export const SideBar = () => {
+  return (
+    <aside className="hidden w-[230px] border-r lg:flex lg:flex-col">
+      <div className="flex h-[104px] items-center gap-2 p-8">
+        <Icons.BankDashIcon />
+        <span className="text-2xl font-bold">BankDash.</span>
+      </div>
+      <nav>
+        <ul className="p-8 pl-0">
+          {menuItems.map((item) => (
+            <ListItem
+              key={item.href}
+              iconName={item.iconName}
+              href={item.href}
+              title={item.title}
+            />
+          ))}
+        </ul>
+      </nav>
+    </aside>
+  );
+};
+
+export const SideBarMobile = () => {
+  return (
+    <>
+      <SheetHeader className="pl-6">
+        <Icons.BankDashMobileIcon />
+        <SheetTitle>BankDash.</SheetTitle>
+      </SheetHeader>
+      <aside className="flex w-[230px]">
+        <ul className="p-8 pl-0">
+          {menuItems.map((item) => (
+            <ListItem
+              key={item.href}
+              iconName={item.iconName}
+              href={item.href}
+              title={item.title}
+            />
+          ))}
+        </ul>
+      </aside>
+    </>
+  );
+};
