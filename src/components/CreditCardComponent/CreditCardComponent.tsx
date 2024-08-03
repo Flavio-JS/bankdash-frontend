9f378d9639a80e583edfc097540ed53e41f7a16f
Flavio-JS <flavim.filho007@gmail.com>
2024-08-03 13:33:04 -0300

    reduced code

--- src/components/CreditCardComponent/CreditCardComponent.tsx
+++ src/components/CreditCardComponent/CreditCardComponent.tsx
@@ -50,56 +50,56 @@ export const CreditCardComponent = ({
   const getMainStyle = () => {
     switch (cardStyle) {
       case "slate":
-        return "bg-gradient-to-tl from-slate-950 to-slate-800 text-white";
+        return "from-slate-950 to-slate-800";
       case "gray":
-        return "bg-gradient-to-tl from-gray-800 to-gray-500 text-white";
+        return "from-gray-800 to-gray-500";
       case "zinc":
-        return "bg-gradient-to-tl from-zinc-800 to-zinc-500 text-white";
+        return "from-zinc-800 to-zinc-500";
       case "neutral":
-        return "bg-gradient-to-tl from-neutral-800 to-neutral-500 text-white";
+        return "from-neutral-800 to-neutral-500";
       case "stone":
-        return "bg-gradient-to-tl from-stone-800 to-stone-500 text-white";
+        return "from-stone-800 to-stone-500";
       case "red":
-        return "bg-gradient-to-tl from-red-800 to-red-500 text-white";
+        return "from-red-800 to-red-500";
       case "orange":
-        return "bg-gradient-to-tl from-orange-800 to-orange-500 text-white";
+        return "from-orange-800 to-orange-500";
       case "amber":
-        return "bg-gradient-to-tl from-amber-800 to-amber-500 text-white";
+        return "from-amber-800 to-amber-500";
       case "yellow":
-        return "bg-gradient-to-tl from-yellow-800 to-yellow-500 text-white";
+        return "from-yellow-800 to-yellow-500";
       case "lime":
-        return "bg-gradient-to-tl from-lime-800 to-lime-500 text-white";
+        return "from-lime-800 to-lime-500";
       case "green":
-        return "bg-gradient-to-tl from-green-800 to-green-500 text-white";
+        return "from-green-800 to-green-500";
       case "emerald":
-        return "bg-gradient-to-tl from-emerald-800 to-emerald-500 text-white";
+        return "from-emerald-800 to-emerald-500";
       case "teal":
-        return "bg-gradient-to-tl from-teal-800 to-teal-500 text-white";
+        return "from-teal-800 to-teal-500";
       case "cyan":
-        return "bg-gradient-to-tl from-cyan-800 to-cyan-500 text-white";
+        return "from-cyan-800 to-cyan-500";
       case "sky":
-        return "bg-gradient-to-tl from-sky-800 to-sky-500 text-white";
+        return "from-sky-800 to-sky-500";
       case "blue":
-        return "bg-gradient-to-tl from-blue-800 to-blue-500 text-white";
+        return "from-blue-800 to-blue-500";
       case "indigo":
-        return "bg-gradient-to-tl from-indigo-800 to-indigo-500 text-white";
+        return "from-indigo-800 to-indigo-500";
       case "violet":
-        return "bg-gradient-to-tl from-violet-800 to-violet-500 text-white";
+        return "from-violet-800 to-violet-500";
       case "purple":
-        return "bg-gradient-to-tl from-purple-800 to-purple-500 text-white";
+        return "from-purple-800 to-purple-500";
       case "fuchsia":
-        return "bg-gradient-to-tl from-fuchsia-800 to-fuchsia-500 text-white";
+        return "from-fuchsia-800 to-fuchsia-500";
       case "pink":
-        return "bg-gradient-to-tl from-pink-800 to-pink-500 text-white";
+        return "from-pink-800 to-pink-500";
       case "purple":
-        return "bg-gradient-to-tl from-purple-800 to-purple-500 text-white";
+        return "from-purple-800 to-purple-500";
     }
   };
 
   return (
     <div
       className={cn(
-        "flex h-[170px] w-[231px] flex-col justify-between rounded-2xl",
+        "flex h-[170px] w-[231px] flex-col justify-between rounded-2xl bg-gradient-to-tl text-white",
         getMainStyle()
       )}
     >
