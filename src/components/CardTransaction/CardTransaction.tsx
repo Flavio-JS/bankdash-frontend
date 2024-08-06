086053201171eb955ab1d20bf2caef0b31069e32
Marcelol090 <rengarmarcelo.mgh@gmail.com>
2024-08-06 19:33:31 -0300

    Finalizado CardTransaction e adicionado icons

--- src/components/CardTransaction/CardTransaction.tsx
+++ src/components/CardTransaction/CardTransaction.tsx
@@ -1,17 +1,25 @@
-import { type Icons } from "@/components/Icons/Icons";
-import { BriefcaseBusinessIcon } from "lucide-react";
+import { Icons } from "@/components/Icons/Icons";
 
+const ValidIcons = {
+  BusinessIcon: Icons.BusinessIcon,
+  PaypalIcon: Icons.PaypalIcon,
+};
 export type CardTransactionProps = {
-  icon: keyof typeof Icons;
+  icon: keyof typeof ValidIcons;
   title: string;
   subTitle: string;
 };
 
-export const CardTransaction = ({ title, subTitle }: CardTransactionProps) => {
+export const CardTransaction = ({
+  icon,
+  title,
+  subTitle,
+}: CardTransactionProps) => {
+  const Icon = Icons[icon];
   return (
     <div className="flex justify-between gap-3 bg-[#F5F7FA] p-3">
       <div>
-        <BriefcaseBusinessIcon />
+        <Icon />
       </div>
       <div className="flex flex-col">
         <span>{title}</span>
