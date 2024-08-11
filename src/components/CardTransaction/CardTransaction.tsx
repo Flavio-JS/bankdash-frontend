3018bed574625bd5d97acf636aa684e8fb7651fb
Flavio-JS <flavim.filho007@gmail.com>
2024-08-11 09:12:53 -0300

    finished CardTransactionSession and CardTransaction

--- src/components/CardTransaction/CardTransaction.tsx
+++ src/components/CardTransaction/CardTransaction.tsx
@@ -1,32 +1,52 @@
 import { Icons } from "@/components/Icons/Icons";
+import { cn } from "@/lib/utils";
 
 const ValidIcons = {
   BusinessIcon: Icons.BusinessIcon,
   PaypalIcon: Icons.PaypalIcon,
+  PaymentIcon: Icons.PaymentIcon,
 };
 export type CardTransactionProps = {
   icon: keyof typeof ValidIcons;
   title: string;
-  subTitle: string;
+  date: string;
+  currency: number;
 };
 
 export const CardTransaction = ({
   icon,
   title,
-  subTitle,
+  date,
+  currency,
 }: CardTransactionProps) => {
   const Icon = Icons[icon];
   return (
-    <div className="flex justify-between gap-3 bg-[#F5F7FA] p-3">
-      <div>
-        <Icon />
+    <div className="flex w-full items-center justify-between gap-3 p-3">
+      <div className="flex gap-3">
+        <div>
+          <Icon />
+        </div>
+        <div className="flex flex-col">
+          <span className="text-sm font-medium text-[#232323] lg:text-base">
+            {title}
+          </span>
+          <span className="text-xs text-[#718EBF] lg:text-sm">{date}</span>
+        </div>
       </div>
-      <div className="flex flex-col">
-        <span>{title}</span>
-        <span>{subTitle}</span>
-      </div>
+
       <div>
-        <span>-850</span>
+        <span
+          className={cn(
+            currency < 0 ? "text-[#FF4B4A]" : "text-[#41D4A8]",
+            "text-sm lg:text-base"
+          )}
+        >
+          {currency > 0 && "+"}
+          {currency.toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD",
+          })}
+        </span>
       </div>
     </div>
   );
---
+++ src/components/CardTransactionSession/CardTransactionSession.tsx
@@ -0,0 +1,46 @@
+import {
+  CardTransaction,
+  type CardTransactionProps,
+} from "@/components/CardTransaction/CardTransaction";
+
+const transactions: CardTransactionProps[] = [
+  {
+    icon: "BusinessIcon",
+    title: "Deposit from my Card",
+    date: "28 January 2021",
+    currency: -850,
+  },
+  {
+    icon: "PaypalIcon",
+    title: "Deposit Paypal",
+    date: "25 January 2021",
+    currency: 2500,
+  },
+  {
+    icon: "PaymentIcon",
+    title: "Jemi Wilson",
+    date: "21 January 2021",
+    currency: 5400,
+  },
+];
+
+export const CardTransactionSession = () => {
+  return (
+    <div className="flex flex-col items-start gap-5">
+      <h2 className="text-2xl font-semibold text-[#343C6A]">
+        Recent Transaction
+      </h2>
+      <div className="rounded-3xl bg-white p-6">
+        {transactions.map((transaction) => (
+          <CardTransaction
+            key={transaction.title}
+            icon={transaction.icon}
+            title={transaction.title}
+            date={transaction.date}
+            currency={transaction.currency}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
