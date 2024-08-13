f6a2b7618c43f81c59039c2ec31ebe33d7d1ca21
Flavio-JS <flavim.filho007@gmail.com>
2024-08-13 19:17:33 -0300

    init QuickTransfer

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,11 @@
-import { BalanceHistoryChart } from "@/features/dashboard/components/BalanceHistoryChart/BalanceHistoryChart";
-import { ChartSession } from "@/features/dashboard/components/ChartSession/ChartSession";
+import { QuickTransfer } from "@/features/dashboard/components/QuickTransfer/QuickTransfer";
 
 export default function Dashboards() {
   return (
     <main className="flex min-h-full max-w-full flex-col items-center">
       <div className="flex w-full items-center justify-center gap-4 p-4">
         <div className="w-full max-w-[700px]">
-          <ChartSession title="Balance History">
-            <BalanceHistoryChart />
-          </ChartSession>
+          <QuickTransfer />
         </div>
       </div>
     </main>
---
+++ src/features/dashboard/components/QuickTransfer/QuickTransfer.tsx
@@ -0,0 +1,87 @@
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { Input } from "@/components/ui/input";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Icons } from "@/features/dashboard/components/Icons/Icons";
+import { Search } from "lucide-react";
+
+const avatarData = [
+  {
+    name: "Jemi Wilson1",
+    image: "https://avatars.githubusercontent.com/u/106037619?v=4",
+  },
+  {
+    name: "Jemi Wilson2",
+    image: "https://avatars.githubusercontent.com/u/106037619?v=4",
+  },
+  {
+    name: "Jemi Wilson3",
+    image: "https://avatars.githubusercontent.com/u/106037619?v=4",
+  },
+];
+
+export const QuickTransfer = () => {
+  return (
+    <div className="flex flex-col items-start gap-5">
+      <h2 className="text-2xl font-semibold text-[#343C6A]">QuickTransfer</h2>
+      <div className="flex flex-col gap-6 rounded-3xl bg-white p-6">
+        <div className="flex items-center justify-center gap-4">
+          {avatarData.map((data) => (
+            <div key={data.name} className="flex flex-col items-center gap-3">
+              <Avatar className="h-9 w-9 lg:h-16 lg:w-16">
+                <AvatarImage src={data.image} alt="avatar image" />
+                <AvatarFallback>FJ</AvatarFallback>
+              </Avatar>
+              <div className="flex flex-col items-center">
+                <span className="text-xs">{data.name}</span>
+                <span className="text-xs text-[#718EBF]">CEO</span>
+              </div>
+            </div>
+          ))}
+
+          <Popover>
+            <PopoverTrigger>
+              <button className="rounded-full bg-white p-3 drop-shadow-2xl">
+                <Search />
+              </button>
+            </PopoverTrigger>
+            <PopoverContent>
+              <div className="flex flex-col gap-4">
+                <div className="flex w-full max-w-sm items-center space-x-2">
+                  <Input
+                    className="focus:border-[#1814F3] focus-visible:ring-transparent"
+                    type="name"
+                    placeholder="Name"
+                  />
+                  <button type="submit">
+                    <Search />
+                  </button>
+                </div>
+              </div>
+            </PopoverContent>
+          </Popover>
+        </div>
+        <div className="flex items-center justify-between gap-5">
+          <span className="text-[#718EBF]">Write Amount</span>
+          <div className="flex w-max items-center justify-between gap-3 rounded-full bg-[#EDF1F7] pl-4">
+            <input
+              className="w-12 bg-transparent text-[#718EBF] focus-visible:outline-none"
+              type="text"
+              name="text"
+              id="text"
+              inputMode="numeric"
+              pattern="[0-9]*"
+            />
+            <button className="flex items-center justify-center rounded-full bg-[#1814F3] px-5 py-3 text-white">
+              <span>Send</span>
+              <Icons.SendIcon />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
